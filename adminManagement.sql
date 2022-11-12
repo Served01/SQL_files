a21b83463ebc5e5d1bc361803b65312a96f74ca7
Lim WooSeong <smnlk4832@gmail.com>
2022-11-13 07:11:26 +0900

    221113_1
    07.11

---
+++ adminManagement.sql
@@ -0,0 +1,9 @@
+delete BOOK_INFO;
+
+select * from BOOK_INFO;
+
+insert into Book_info values(12345678, '제목1', '작성자1', '출판사1', '2022-11-07', 'bk_image.jpg', '해외', '인문', sysdate, '가나다라마바사1', 5, 50, '제목1', 0);
+insert into Book_info values(12345679, '제목2', '작성자2', '출판사2', '2022-11-08', 'bk_image.jpg', '해외', '인문', sysdate, '가나다라마바사2', 5, 50, '제목2', 0);
+insert into Book_info values(12345670, '제목3', '작성자3', '출판사3', '2022-11-09', 'bk_image.jpg', '해외', '인문', sysdate, '가나다라마바사3', 5, 50, '제목3', 0);
+
+commit
